9e844b70c1dc3e852c2e8151fee4e166fa7737f0
JahnieTurner <80420943+JahnieTurner@users.noreply.github.com>
2022-06-29 18:05:53 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,36 @@
+video = "";
 object_status = "";
+object = [];
 
 function setup() {
-    canvas = createCanvas(380, 380);
+    canvas = createCanvas(480, 380);
     canvas.center();
-    video = createCapture(VIDEO);
-    video.hide();
-    video.size(380, 380);
+}
+
+function draw() {
+    image(video, 0, 0, 480, 380);
+    if (object_status != "") {
+        objectDetector.detect(video, gotResults);
+        for (i = 0; i < object.length; i++) {
+            document.getElementById("status").innerHTML = "status: objects detected";
+            document.getElementById("number_of_objects").innerHTML = "number of objects detected are- " + object.length;
+            fill('#FF0000');
+            percent = floor(object[i].confidence * 100);
+            text(object[i].label + ' ' + percent + '%', object[i].x + 15, object[i].y + 15);
+            noFill();
+            stroke('#FF0000');
+            rect(object[i].x, object[i].y, object[i].width, object[i].length);
+        }
+    }
+}
+
+function gotResults(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        object = results;
+    }
 }
 
 function start() {
@@ -16,8 +41,4 @@ function start() {
 function modelLoaded() {
     console.log("model loaded");
     object_status = true;
-}
-
-function draw() {
-    image(video, 0, 0, 380, 380)
 }
